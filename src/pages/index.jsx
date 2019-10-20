fb752262d0452970d926ffafca514a67d5780f44
Matheus Alves <matheus.henriquesouzaalves@gmail.com>
2019-10-20 01:50:01 -0400

    example: pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Hello World ! (Home)</h1>
+      <a href="/about">Ir para About</a>
+    </div>
+  );
+}
+
+export default Home; 
